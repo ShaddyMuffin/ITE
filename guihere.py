235f6fe753708c749ffe51c8fca3406c5c806fd9
Vishal Shekha <88841566+ShaddyMuffin@users.noreply.github.com>
2023-11-04 00:43:51 +0530

    Update guihere.py
    Debugged

--- guihere.py
+++ guihere.py
@@ -1,27 +1,38 @@
-def callit(new_text):
+def callit():
     import tkinter as tk
     from tkinter import PhotoImage
     import ttv
 
-    def update_label():
-        label.config(text=new_text)  # Update the text label with the new text
+    # Function to update the text in the label and call another function
+    def update_text():
+        new_text = get_new_text()  # Call a function to get new text
+        label.config(text=new_text)  # Update the text in the label
         root.update()
-        ttv.videoPlayer(new_text)
-    
+        after_update_function(new_text)  # Call another function after updating the text
+
+    # Function to get new text (replace this with your logic to generate new text)
+    def get_new_text():
+        return ttv.run_model()
+
+    # Function to be called after updating the text (replace this with your desired function)
+    def after_update_function(n):
+        ttv.videoPlayer(n)
+
+
     # Create the main window
     root = tk.Tk()
-    root.title("Image Button and Dynamic Text Label")
+    root.title("Dynamic Text Label")
 
     # Load an image for the button
-    image = PhotoImage(file="C:\\Users\\Admin\\OneDrive\\Pictures\\Camera Roll\\download.png")
+    button_image = PhotoImage(file="C:\\Users\\Admin\\OneDrive\\Pictures\\Camera Roll\\download.png")
 
     # Create an image button
-    image_button = tk.Button(root, image=image, command=update_label)
+    image_button = tk.Button(root, image=button_image, command=update_text)
     image_button.pack(side=tk.TOP, padx=10, pady=10)
 
-    # Create a dynamic text label
-    label = tk.Label(root, text="")
-    label.pack(side=tk.TOP, padx=10, pady=10)
-    
-    # Run the tkinter main loop
+    # Create a label with initial text
+    label = tk.Label(root, text="", font=("Arial", 14))
+    label.pack(side=tk.TOP,padx=10, pady=10)
+
+    # Start the main loop
     root.mainloop()
