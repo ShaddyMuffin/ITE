ff7e84ae6ccdcf0bf61fe70843ed9bd5464b869f
Vishal Shekha <88841566+ShaddyMuffin@users.noreply.github.com>
2023-11-30 22:19:01 +0530

    Update guihere.py

--- guihere.py
+++ guihere.py
@@ -2,32 +2,24 @@ def callit():
     import tkinter as tk
     from tkinter import PhotoImage
     import ttv
+    import stt
 
     # Function to update the text in the label and call another function
     def update_text():
-        new_text = get_new_text()  # Call a function to get new text
-        label.config(text=new_text)  # Update the text in the label
-        root.update()
-        after_update_function(new_text)  # Call another function after updating the text
-
-    # Function to get new text (replace this with your logic to generate new text)
-    def get_new_text():
-        return ttv.run_model()
-
-    # Function to be called after updating the text (replace this with your desired function)
-    def after_update_function(n):
-        ttv.videoPlayer(n)
-
+        new_text = stt.convt()  # Call a function to get new text
+        label.config(text=new_text)  
+        root.update() # Update the text in the label
+        ttv.videoPlayer(new_text)
 
     # Create the main window
     root = tk.Tk()
-    root.title("Dynamic Text Label")
+    root.title("Speech to Text")
 
     # Load an image for the button
     button_image = PhotoImage(file="C:\\Users\\Admin\\OneDrive\\Pictures\\Camera Roll\\download.png")
 
     # Create an image button
-    image_button = tk.Button(root, image=button_image, command=update_text)
+    image_button = tk.Button(root, image=button_image, command=update_text) # command = function , the function is called when the button is clicked
     image_button.pack(side=tk.TOP, padx=10, pady=10)
 
     # Create a label with initial text
@@ -35,4 +27,6 @@ def callit():
     label.pack(side=tk.TOP,padx=10, pady=10)
 
     # Start the main loop
+    print("Hit the button to Start the communication!".center(80))
+    print()
     root.mainloop()
